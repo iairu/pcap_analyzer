cd050d5dac0ff43243181c9e55afc838f7abe404
Ondrej Spanik <spanik11@msn.com>
2021-10-11 09:23:10 +0200

    Implemented scapy PCAP reading, iteration and raw bytes printout

--- __main__.py
+++ __main__.py
@@ -1,11 +1,15 @@
 from _close import *
 from _args import *
+import scapy.all as scapy
 
 def main():
     args = Args()
-    
-    print("Testing some printing " + args.count.__str__() + " and the path is ...")
-    print(args.path)
+    packets = scapy.rdpcap(args.path, args.count)
+
+    count = len(packets) # packets[0] is first if count > 0, packets[count-1] is last
+    for i in range(count):
+        print("Frame #" + str(i + 1) + ":")
+        print(bytes(packets[i])) # todo this will go into Analyzer(raw_frame) after i figure out what some symbols in the bytes print sequence mean
 
     close(Code.SUCCESS)
 
