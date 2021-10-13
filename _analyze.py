f807e6775a66b8c96e876425757db9ed33665e9f
Ondrej Spanik <spanik11@msn.com>
2021-10-13 12:31:37 +0200

    Shortened SAP/ETHTYPE dict names, novell->ieee raw

--- _analyze.py
+++ _analyze.py
@@ -9,7 +9,7 @@ class Analyze:
     class Eth_stds:
         """ Supported Ethernet Standards """
         UNKNOWN = "Unknown"
-        NOVELL_802_3_RAW = "Novell 802.3 RAW"           # todo: check 802.2 802.3 if correct minor ver.
+        IEEE_802_3_RAW = "IEEE 802.3 RAW"           # todo: check 802.2 802.3 if correct minor ver.
         IEEE_802_2_LLCSNAP = "IEEE 802.2 LLC+SNAP"  # todo: check 802.2 802.3 if correct minor ver.
         IEEE_802_2_LLC = "IEEE 802.2 LLC"
         ETHERNET2 = "Ethernet II"
@@ -17,37 +17,37 @@ class Analyze:
     class __Dict__:
         saps = { # todo: nacitat zo suboru
             0x00: "Null SAP",
-            0x02: "LLC Sublayer Management / Individual",
-            0x03: "LLC Sublayer Management / Group",
-            0x06: "IP (DoD Internet Protocol)",
-            0x0E: "PROWAY (IEC 955) Network Management, ...",
-            0x42: "BPDU (Bridge PDU / 802.1 Spanning Tree)",
-            0x4E: "MMS (Manufacturing Message Service)",
+            0x02: "LLC SM / Individual",
+            0x03: "LLC SM / Group",
+            0x06: "IP (DoDIP)",
+            0x0E: "PROWAY Net",
+            0x42: "BPDU / 802.1 STree",
+            0x4E: "MMS",
             0x5E: "ISI IP",
             0x7E: "X.25 PLP (ISO 8208)",
-            0x8E: "PROWAY (IEC 955) ASL Maintenance",
-            0xAA: "SNAP (Sub-net Access Protocol)",
-            0xE0: "IPX (Novell NetWare)",
+            0x8E: "PROWAY ASL",
+            0xAA: "SNAP",
+            0xE0: "IPX",
             0xF4: "LAN Management",
-            0xFE: "ISO Network Layer Protocols",
+            0xFE: "ISO Net Layer Protocols",
             0xFF: "Global DSAP"
         }
         eth_types = { # todo: nacitat zo suboru
             0x0200: "XEROX PUP",
             0x0201: "PUP Addr Trans",
-            0x0800: "Internet IP (IPv4)",
+            0x0800: "IPv4",
             0x0801: "X.75 Internet",
             0x0805: "X.25 Level 3",
-            0x0806: "ARP (Address Resolution Protocol)",
+            0x0806: "ARP",
             0x8035: "Reverse ARP",
             0x809B: "Appletalk",
             0x80F3: "AppleTalk AARP (Kinetics)",
             0x8100: "IEEE 802.1Q VLAN-tagged frames",
-            0x8137: "Novell IPX",
+            0x8137: "IPX",
             0x86DD: "IPv6",
             0x880B: "PPP",
             0x8847: "MPLS",
-            0x8848: "MPLS with upstream-assigned label",
+            0x8848: "MPLS upstream label",
             0x8863: "PPPoE Discovery Stage",
             0x8864: "PPPoE Session Stage"
         }
@@ -85,7 +85,7 @@ class Analyze:
             # 802.2 SNAP (== LLC + SNAP)    0xAAAA
             # 802.2 LLC                     0x____
             if (dsap_or_raw == "ffff"):
-                self.eth_std = self.Eth_stds.NOVELL_802_3_RAW
+                self.eth_std = self.Eth_stds.IEEE_802_3_RAW
             elif (dsap_or_raw == "aaaa"):
                 self.eth_std = self.Eth_stds.IEEE_802_2_LLCSNAP
             else:
@@ -108,7 +108,7 @@ class Analyze:
             return
 
         # LOGICAL LINK HEADER +
-        # if (self.eth_std == self.Eth_stds.NOVELL_802_3_RAW):
+        # if (self.eth_std == self.Eth_stds.IEEE_802_3_RAW):
         #     self.is_ipx = True if (_bytes[14:16].hex() == "ffff") else False # ffff checksum 14-15 bajt (2B)    # <--- obvious if RAW
         if (self.eth_std == self.Eth_stds.IEEE_802_2_LLC or self.eth_std == self.Eth_stds.IEEE_802_2_LLCSNAP):
             self.dsap = _bytes[14:15] # (1B)
@@ -140,8 +140,9 @@ class Analyze:
         else:
             print(f"\_ Length:            0x{self.eth_len.hex()} [{btoi(self.eth_len)}]")
 
-            if (self.eth_std == self.Eth_stds.NOVELL_802_3_RAW):
-                print(f"IPX")
+            if (self.eth_std == self.Eth_stds.IEEE_802_3_RAW):
+                """soon"""
+                #print(f"IPX")
             else:
                 print(f"LOGICAL LINK Header:")
                 print(f"\_ DSAP:              0x{self.dsap.hex()} [{self.str_sap(btoi(self.dsap))}]")
