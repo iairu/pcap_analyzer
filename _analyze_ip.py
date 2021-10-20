1180414674586e4e64a87147a6ea5a25f87acef6
Ondrej Spanik <spanik11@msn.com>
2021-10-20 16:33:11 +0200

    _analyze_ip: Prep for TCP.. analysis: save data...

--- _analyze_ip.py
+++ _analyze_ip.py
@@ -10,14 +10,17 @@ class AnalyzeIP:
     # ------------------------------------------
 
     def str_ip(self, code: int) -> str:
-        return self._ports.str_ip(code)
+        return self._protocols.str_ip(code)
 
-    def __init__(self, _data: bytes, ports: Protocols):
-        self._ports = ports
+    def __init__(self, _data: bytes, protocols: Protocols):
+        self._protocols = protocols
 
         self.protocol = _data[9:10] # 6.bajt od zaciatku IPv4 (1B)
+        self.protocol_str = self.str_ip(btoi(self.protocol))
+
         self.ip_src = _data[12:16] # 12-15 bajt (4B)
         self.ip_dst = _data[16:20] # 16-19 bajt (4B)
+        self.data = _data[20:] # 20+ bajt
         return
 
     def output(self):
@@ -26,7 +29,7 @@ class AnalyzeIP:
 
     def print(self):
         print(f"IPv4")
-        print(f"\_ Protocol:          0x{self.protocol.hex()} [{self.str_ip(btoi(self.protocol))}]")
+        print(f"\_ Protocol:          0x{self.protocol.hex()} [{self.protocol_str}]")
         print(f"\_ Source IP:         {delim(self.ip_src)} [{btoIPv4(self.ip_src)}]")
         print(f"\_ Dest   IP:         {delim(self.ip_dst)} [{btoIPv4(self.ip_dst)}]")
         return
